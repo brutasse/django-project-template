6d4ad770502aa6cf2ff94adc24f0039bb1ab78b3
Bruno Renié <buburno@gmail.com>
2012-09-30 19:23:37 +0200

    Enabled the debug toolbar on DEBUG=True

--- project_name/settings.py
+++ project_name/settings.py
@@ -173,3 +173,38 @@ LOGGING = {
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = os.path.join(HERE, os.pardir)
 TEST_DISCOVER_ROOT = os.path.join(TEST_DISCOVER_TOP_LEVEL, 'tests')
+
+if DEBUG:
+    try:
+        import debug_toolbar  # noqa
+    except ImportError:
+        pass
+    else:
+        INTERNAL_IPS = (
+            '127.0.0.1',
+        )
+
+        INSTALLED_APPS += (
+            'debug_toolbar',
+        )
+
+        MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+            'debug_toolbar.middleware.DebugToolbarMiddleware',
+        )
+
+        DEBUG_TOOLBAR_CONFIG = {
+            'INTERCEPT_REDIRECTS': False,
+            'HIDE_DJANGO_SQL': False,
+        }
+
+        DEBUG_TOOLBAR_PANELS = (
+            'debug_toolbar.panels.version.VersionDebugPanel',
+            'debug_toolbar.panels.timer.TimerDebugPanel',
+            'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+            'debug_toolbar.panels.headers.HeaderDebugPanel',
+            'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+            'debug_toolbar.panels.template.TemplateDebugPanel',
+            'debug_toolbar.panels.sql.SQLDebugPanel',
+            'debug_toolbar.panels.signals.SignalDebugPanel',
+            'debug_toolbar.panels.logger.LoggingPanel',
+        )
