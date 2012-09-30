ee236fea9fe5774779a9ec790b7b70c77370cdb3
Bruno Renié <buburno@gmail.com>
2012-09-30 21:34:46 +0200

    Added static and media serving to the URLconf for dev

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from ratelimitbackend import admin
 admin.autodiscover()
@@ -6,3 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
