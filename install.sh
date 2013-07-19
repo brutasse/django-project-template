b2b2582c0f13997e4eb92874956431a75da157f6
Nicolas Dubois <nicolas.c.dubois@gmail.com>
2013-07-19 20:57:30 +0200

    Fixed script (using mv instead of cp)

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 for template in $(find . -iname "*.template"); do 
-    cp -v $template $(dirname $template)/$(basename $template .template)
+    mv -v $template $(dirname $template)/$(basename $template .template)
 done
 pip install -r requirements.txt && pip freeze -l > requirements.txt
 rm -f install.sh
