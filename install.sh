79c952e7ee006377682227fd9b4a68b1a7473d75
Bruno Renié <brutasse@gmail.com>
2013-10-22 17:07:27 +0200

    Freeze requirements-dev as well

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ for template in $(find . -iname "*.template"); do
     mv -v $template $(dirname $template)/$(basename $template .template)
 done
 pip install -r requirements.txt && pip freeze -l > requirements.txt
+pip install -r requirements-dev.txt
+comm -23 <(pip freeze -l | sort -n) <(cat requirements.txt | sort -n) > requirements-dev.txt
+sed -i '1s/^/-r requirements.txt\n/' requirements-dev.txt
 rm -f install.sh
 
